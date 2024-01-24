4fe19aeb7a7eb10aaa2285e42ada6a33d71f1e3f
Luke Ren <luke.ren@mail.utoronto.ca>
2024-01-24 14:04:32 -0500

    update fetch helper function

--- backend/helpers/fetchHelper.js
+++ backend/helpers/fetchHelper.js
@@ -17,7 +17,7 @@ async function fetchDataTypeInterfaces(name, req, res) {
   }
 
   const interfaces = {}
-  objects.map(object => interfaces[object._uri] = object.name || 'Not Given')
+  objects.map(object => interfaces[object._uri] = object.name || 'Name Not Given')
   return res.status(200).json({success: true, interfaces});
 }
 
