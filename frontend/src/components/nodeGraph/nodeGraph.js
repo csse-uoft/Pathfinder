46e90ca54d5a46b9d33619562460a953e0eb44b4
Luke Ren <luke.ren@mail.utoronto.ca>
2024-07-25 23:46:10 -0400

    setup node graph frontend

--- frontend/src/components/nodeGraph/nodeGraph.js
+++ frontend/src/components/nodeGraph/nodeGraph.js
@@ -1,3 +1,77 @@
+import React, {useContext, useRef, useEffect, useState} from 'react';
+import cytoscape from 'cytoscape';
+import ReactDOM from "react-dom/client";
+import {Container} from "@mui/material";
+import {makeStyles} from "@mui/styles";
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '80%'
+  },
+  button: {
+    marginTop: 12,
+    marginBottom: 0,
+    length: 100
+  },
+}));
+
 export default function NodeGraph() {
 
+
+  const classes = useStyles();
+
+  const cyRef = useRef(null);
+
+  useEffect(() => {
+    // Initialize Cytoscape
+    cyRef.current = cytoscape({
+      container: document.getElementById('cy'), // container to render in
+      elements: [ // list of graph elements to start with
+        { // node a
+          data: { id: 'a' }
+        },
+        { // node b
+          data: { id: 'b' }
+        },
+        { // edge ab
+          data: { id: 'ab', source: 'a', target: 'b' }
+        }
+      ],
+      style: [ // the stylesheet for the graph
+        {
+          selector: 'node',
+          style: {
+            'background-color': '#666',
+            'label': 'data(id)'
+          }
+        },
+        {
+          selector: 'edge',
+          style: {
+            'width': 3,
+            'line-color': '#ccc',
+            'target-arrow-color': '#ccc',
+            'target-arrow-shape': 'triangle'
+          }
+        }
+      ],
+      layout: {
+        name: 'grid',
+        rows: 1
+      }
+    });
+
+    // Cleanup on component unmount
+    return () => {
+      cyRef.current.destroy();
+    };
+  }, []);
+
+  return (
+    <Container sx={{alignItems: 'center'}}>
+      <div id="cy" style={{width: '80vw', height: '80vh'}}>
+
+      </div>
+    </Container>
+
+  );
 }
