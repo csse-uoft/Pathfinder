0b9ab55c47dc5f2bf32964245553b02830920d00
Luke Ren <luke.ren@mail.utoronto.ca>
2024-10-22 11:01:46 -0400

    fix error on file uploading

--- backend/services/fileUploading/configs.js
+++ backend/services/fileUploading/configs.js
@@ -25,7 +25,7 @@ const fullLevelConfig = {
   organization: {},
   outcome: {
     // 'cids:hasIndicator': {flag: true},
-    'oep:partOf': {ignoreInstance: true}
+    // 'oep:partOf': {ignoreInstance: true}
   },
   impactNorms: {},
   indicator: {
